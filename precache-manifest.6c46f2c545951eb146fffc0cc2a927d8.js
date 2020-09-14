6236497b33ebd489c8ecb6bd15ab2c3aa4e4e62b
suryan535 <suryan535@gmail.com>
2020-09-14 21:28:29 +0530

    Updates

---
+++ precache-manifest.6c46f2c545951eb146fffc0cc2a927d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20377db901deae7c02807d6f8f2816fd",
+    "url": "/suryan535/LoginPage/index.html"
+  },
+  {
+    "revision": "6d829f4e2ac1f9acb222",
+    "url": "/suryan535/LoginPage/static/css/main.a91e0d57.chunk.css"
+  },
+  {
+    "revision": "59a837049ed2927a92d9",
+    "url": "/suryan535/LoginPage/static/js/2.30bd503c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/suryan535/LoginPage/static/js/2.30bd503c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d829f4e2ac1f9acb222",
+    "url": "/suryan535/LoginPage/static/js/main.d2b13771.chunk.js"
+  },
+  {
+    "revision": "45cd2b782b93b3b0bcfd",
+    "url": "/suryan535/LoginPage/static/js/runtime-main.e03c0c85.js"
+  },
+  {
+    "revision": "76d14216f0fe1455343657b2b75a9b91",
+    "url": "/suryan535/LoginPage/static/media/Background2.76d14216.svg"
+  }
+]);
